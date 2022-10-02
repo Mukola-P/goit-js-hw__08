1d05f31cc2a50068e82dbf52f98a6887363fd35f
Mukola-P <proxorov.mukola@gmail.com>
2022-10-02 21:15:28 +0300

    task_1__3-finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash/throttle';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(inputElText, 500));
+form.addEventListener('submit', submitElForm);
+const INPUT_KEY = 'feedback-form-state';
+let objectInput = {};
+
+inForm();
+
+function submitElForm(e) {
+  e.preventDefault();
+  const formEl = e.target.elements;
+  const email = formEl.email.value;
+  const message = formEl.message.value;
+    if (message === '' || email ==="") {
+       return alert(`Поля "email" та "message" мають бути заповнені!`);
+    }
+ 
+  objectInput = {
+    email,
+    message,
+  };
+  console.log(objectInput);
+
+  form.reset();
+  localStorage.removeItem(INPUT_KEY);
+}
+
+function inputElText(e) {
+  objectInput[e.target.name] = e.target.value;
+  localStorage.setItem(INPUT_KEY, JSON.stringify(objectInput));
+}
+
+function inForm() {
+  let saveText = localStorage.getItem(INPUT_KEY);
+  if (saveText) {
+    saveText = JSON.parse(saveText);
+    Object.entries(saveText).forEach(([name, value]) => {
+      objectInput[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
