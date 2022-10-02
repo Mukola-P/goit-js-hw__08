90c1c4fcabebab14291f59b8d5dcccc3f59d1b0d
Mukola-P <proxorov.mukola@gmail.com>
2022-10-02 20:06:02 +0300

    task-1__2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function galleryMarkup(items) {
+  const markup = items
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`;
+    })
+    .join('');
+
+  galleryRef.innerHTML = markup;
+}
+console.log(galleryRef);
+
+galleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
